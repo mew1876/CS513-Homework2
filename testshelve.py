b8df4b5fe0fa830d47f9290d5e295c92a1f764cf
jujubochnak <jujubochnak@github.com>
2018-03-07 19:30:54 -0600

    using shelve
    made a new test file that runs on shelve rather than pickle.

---
+++ testshelve.py
@@ -0,0 +1,27 @@
+import csv
+import time
+import shelve
+import os.path
+import sys
+
+from Probe import Probe
+from ProbePoint import ProbePoint
+
+probes = []
+if not os.path.isfile('ProbePointsShelf.dat'):	# build the shelf
+	# Read CSV
+	start = time.perf_counter();
+	with shelve.open('ProbePointsShelf', writeback=True) as probeDB:
+		print("creating shelf...")
+		with open('../Partition6467ProbePoints.csv', newline='') as csvFile:
+			reader = csv.reader(csvFile)
+			for row in reader:
+				# print("row: " + str(row))
+				rowProbeID = row[0]
+				if rowProbeID not in probeDB:
+					probeDB[rowProbeID] = []
+				probeDB[rowProbeID].append(row)
+		probeDB.close()
+	print("Loaded data from CSV into a shelf", time.perf_counter() - start, "seconds")
+# oops I accidentally built a shelf
+
