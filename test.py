25480c0c209eec3c4f18a50356d2ec5455aa4941
jujubochnak <jujubochnak@github.com>
2018-03-08 19:54:37 -0600

    Added kilometerDistanceFromPointToPoint

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ def mapMatch(probePoint):
 		bestLink = None
 		for link in linkDB[f"{gridX},{gridY}"] + linkDB[f"{gridX+neighborX},{gridY+neighborY}"] + linkDB[f"{gridX+neighborX},{gridY}"] + linkDB[f"{gridX},{gridY+neighborY}"]:
 			for i in range(0, len(link.shapeInfo) - 1):
-				currentDist = distToLine(link.shapeInfo[i], link.shapeInfo[i+1], [probePoint.latitude, probePoint.longitude])
+				currentDist = distanceFromPointToSegment(link.shapeInfo[i], link.shapeInfo[i+1], [probePoint.latitude, probePoint.longitude])
 				if currentDist > topScore:
 					topScore = currentDist
 					bestLink = link
@@ -38,10 +38,33 @@ def distanceFromPointToSegment(segmentStart, segmentEnd, point): # point = point
 	t = max(0, min(1, t))
 	return math.sqrt(distanceBetweenPointsSquared(point, [segmentEnd[0] + t * (segmentStart[0] - segmentEnd[0]), segmentEnd[1] + t * (segmentStart[1] - segmentEnd[1])]))
 
+def kilometerDistanceFromPointToPoint(point1, point2):
+	# approximate radius of earth in km
+	R = 6373.0
+
+	lat1 = math.radians(point1[0])
+	lon1 = math.radians(point1[1])
+	lat2 = math.radians(point2[0])
+	lon2 = math.radians(point2[1])
+
+	dlon = lon2 - lon1
+	dlat = lat2 - lat1
+
+	a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+	c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+	distance = R * c
+
+	# print("Result:", distance)
+	# print("Should be:", 278.546, "km")
+
+	return distance
+
 def angleBetween(p1, p2, p3, p4): # angle between p2 - p1 and p4 - p3
 	pass
 
-with shelve.open('ProbePointsShelf', writeback=True) as probeDB:
-	probePoint = probeDB["3496"][0]
-	mapMatch(probePoint)
-	# print(distanceFromPointToSegment([3,3],[-1,3],[0,1],))
+# with shelve.open('ProbePointsShelf', writeback=True) as probeDB:
+# 	probePoint = probeDB["3496"][0]
+# 	mapMatch(probePoint)
+# 	# print(distanceFromPointToSegment([3,3],[-1,3],[0,1],))
+# 	print(kilometerDistanceFromPointToPoint("",""))
